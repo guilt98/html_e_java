5e862fc9b5c8dcc12bede167aa8df41cce1360ca
guilt98 <124409817+guilt98@users.noreply.github.com>
2023-05-15 21:52:54 -0300

    Add files via upload

---
+++ parOUimpar.js
@@ -0,0 +1,9 @@
+let n = window.prompt("Digite um numero");// Recebera um numero para N.
+   if (n%2===0) {// Se for divisivel por 2 restando 
+      alert('o numero é par');//Dentro da condição(verdadeiro).
+   }else{// Senão.
+    alert('O numero é impar');// Fora da condição.
+   }
+alert(n);
+
+   
